2825892944dcad820f1b02cb35efaba6eff22d47
George <gchatzinis@gmail.com>
2022-02-15 16:42:49 +0100

    Dependent dropdown

--- stock_app.py
+++ stock_app.py
@@ -11,7 +11,7 @@ st.sidebar.info('This App is created to keep track of Stock Prices and Returns')
 
 st.title('Stock Dashboard App')
 stocks = ('GOOG', 'MSFT', 'DAR', 'AMD', 'ADBE','ABBV','AMZN','PG','PYPL','MU','NEE')
-dropdown =st.multiselect('Select your stocks', stocks, default = ['GOOG', 'MSFT', 'DAR', 'AMD', 'ADBE','ABBV','AMZN','PG','PYPL','MU','NEE'])
+dropdown =st.multiselect('Select at least two stocks', stocks, default = ['GOOG', 'MSFT', 'DAR', 'AMD', 'ADBE','ABBV','AMZN','PG','PYPL','MU','NEE'])
 
 start_date = pd.to_datetime('2020-01-01')
 today = pd.to_datetime('today')
@@ -19,26 +19,26 @@ today = pd.to_datetime('today')
 start = st.date_input('Start', value =start_date)
 end = st.date_input('End', value =today)
 
-if len(dropdown)>0:
-	data = yf.download(dropdown, start, end)['Adj Close']
-	data_for_table = data
+if len(dropdown)>=1:
+	data_close = yf.download(dropdown, start, end)['Adj Close']
+	data_for_table = data_close
 
 # Adj-Close Prices Table
 st.subheader(' Adj-Close Prices')
 st.dataframe(data=data_for_table) # inlcudes the index 0,1,... in the Table
 
 # normalize data to 100 (Pt/P0)*100
-norm_data = data/data.iloc[0]*100
+norm_data = data_close/data_close.iloc[0]*100
 st.subheader('Prices Normalized to 100')
 st.line_chart(norm_data)
 
 # calculate simple returns and plot -> (P1-P0)/P0
-returns = (data / data.shift(1))-1
+returns = (data_close / data_close.shift(1))-1
 st.subheader('Daily Returns')
 st.line_chart(returns)
 ## Histogram
-st.subheader(' Histogram of Daily Returns and Summary Statistics')
-dropdown_2 =st.selectbox('Select your stocks', stocks)
+st.subheader('Histogram of Daily Returns and Summary Statistics')
+dropdown_2 =st.selectbox('Select your stocks', dropdown)
 sns.set_style("darkgrid")
 fig = plt.figure(figsize=(12, 8))
 sns.distplot(returns[dropdown_2])
@@ -55,7 +55,7 @@ def relative_returns(data_):
 	cumret = cumret.fillna(0)
 	return cumret
 st.subheader('Cumulative Returns')
-st.line_chart(relative_returns(data))
+st.line_chart(relative_returns(data_close))
 
 # correlation between the stock returns
 st.subheader('Stock Returns Correlations')
